5917068cedf40bb518ccd8be04b3dadad0924487
Stephan Yaz <syazvinski@gmail.com>
2024-01-17 18:50:44 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -33,35 +33,35 @@ function App() {
   return (
     <div className="bg-[#2789D8] text-white font-inter min-h-screen flex flex-col items-center justify-center px-4">
       <div className="text-center mt-10 md:mt-20">
-        <h1 className="text-3xl md:text-6xl font-bold mb-2">Get letter grades in Altitude Learning</h1>
-        <p className="text-lg md:text-xl">for students of The Mount Vernon School</p>
+        <h1 className="text-2xl md:text-6xl font-bold mb-2">Get letter grades in Altitude Learning</h1>
+        <p className="text-sm md:text-lg">for students of The Mount Vernon School</p>
       </div>
 
       {/* Carousel */}
       <div className="my-10 w-full flex justify-center items-center">
         <div className="hover:bg-gray-300 rounded-full p-2 inline-flex items-center justify-center transition duration-300 mx-2">
-          <button onClick={handlePrev} className="text-2xl md:text-3xl">&laquo;</button>
+          <button onClick={handlePrev} className="text-xl md:text-2xl">&laquo;</button>
         </div>
-        <img src={process.env.PUBLIC_URL + '/images/' + images[currentImageIndex]} alt="Carousel" className="rounded border-4 border-white max-w-[80vw] md:max-w-[40vw] max-h-[40vw]" />
+        <img src={process.env.PUBLIC_URL + '/images/' + images[currentImageIndex]} alt="Carousel" className="rounded border-4 border-white max-w-[60vw] md:max-w-[30vw] max-h-[30vw]" />
         <div className="hover:bg-gray-300 rounded-full p-2 inline-flex items-center justify-center transition duration-300 mx-2">
-          <button onClick={handleNext} className="text-2xl md:text-3xl">&raquo;</button>
+          <button onClick={handleNext} className="text-xl md:text-2xl">&raquo;</button>
         </div>
       </div>
 
       {/* Image Previews */}
       <div className="flex justify-center space-x-2 mb-16">
         {images.map((image, index) => (
-          <img key={index} src={process.env.PUBLIC_URL + '/images/' + image} alt="Preview" onClick={() => handleImageClick(index)} className="w-12 h-auto cursor-pointer" />
+          <img key={index} src={process.env.PUBLIC_URL + '/images/' + image} alt="Preview" onClick={() => handleImageClick(index)} className="w-10 h-auto cursor-pointer" />
         ))}
       </div>
 
       {/* Explanation */}
-      <p className="text-center text-lg md:text-xl mb-4">Click on the button below to be taken to the Chrome Web Store to install:</p>
+      <p className="text-center text-sm md:text-lg mb-4">Click on the button below to be taken to the Chrome Web Store to install:</p>
 
       <div className="flex flex-col items-center mb-4">
         {/* Install Button */}
         <a href="https://chromewebstore.google.com/detail/mount-vernon-school-altit/npejnoaeodlhhllfjdnddllhlegapdeg?hl=en">
-          <img src={process.env.PUBLIC_URL + '/images/webstore_button.png'} alt="Install on Chrome Web Store" className="mt-4 hover:opacity-80 transition duration-200" style={{ width: '200px', height: 'auto', borderRadius: '10px' }} />
+          <img src={process.env.PUBLIC_URL + '/images/webstore_button.png'} alt="Install on Chrome Web Store" className="mt-4 hover:opacity-80 transition duration-200" style={{ width: '150px', height: 'auto', borderRadius: '10px' }} />
         </a>
 
         {/* GitHub Links */}
@@ -76,7 +76,7 @@ function App() {
 
       {/* Footer */}
       <footer className="mt-auto py-4 flex flex-col justify-center items-center">
-        <p className="text-sm mb-2 text-center">made because you shouldn't need a chart to know your grade</p>
+        <p className="text-xs mb-2 text-center">because you shouldn't have to use a chart to see your grade</p>
         <div className="flex justify-center items-center space-x-4">
           <p className="text-center">Made by former student, Stephan Yazvinski. &copy; 2024</p>
           <a href="https://www.linkedin.com/in/stephan-yazvinski-2086771b8/" target="_blank" rel="noopener noreferrer" className="underline text-blue-200">LinkedIn</a>
