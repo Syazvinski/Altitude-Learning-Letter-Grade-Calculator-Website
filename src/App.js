14e1b57e771dec239c078f2a769647f16bf6187d
Stephan Yaz <syazvinski@gmail.com>
2024-01-17 13:09:25 -0500

    update

--- src/App.js
+++ src/App.js
@@ -47,7 +47,6 @@ function App() {
           <button onClick={handleNext} style={{ fontSize: '24px' }}>&raquo;</button>
         </div>
       </div>
-
       {/* Image Previews */}
       <div className="flex justify-center space-x-2">
         {images.map((image, index) => (
@@ -58,15 +57,23 @@ function App() {
       {/* Explanation */}
       <p className="text-center text-xl mt-10">Click on the button below to be taken to the Chrome Web Store to install:</p>
 
-      {/* Install Button */}
-      <a href="https://chromewebstore.google.com/detail/mount-vernon-school-altit/npejnoaeodlhhllfjdnddllhlegapdeg?hl=en">
-        <img src={process.env.PUBLIC_URL + '/images/webstore_button.png'} alt="Install on Chrome Web Store" className="mt-4 hover:opacity-80 transition duration-200" style={{ width: '200px', height: 'auto', borderRadius: '10px' }} />
-      </a>
+      <div className="flex flex-col items-center">
+        {/* Install Button */}
+        <a href="https://chromewebstore.google.com/detail/mount-vernon-school-altit/npejnoaeodlhhllfjdnddllhlegapdeg?hl=en">
+          <img src={process.env.PUBLIC_URL + '/images/webstore_button.png'} alt="Install on Chrome Web Store" className="mt-4 hover:opacity-80 transition duration-200" style={{ width: '200px', height: 'auto', borderRadius: '10px' }} />
+        </a>
+
+        <div className="flex flex-col sm:flex-row justify-center items-center space-y-2 sm:space-y-0 sm:space-x-4 text-xs text-white mt-2 max-w-[200px] text-center whitespace-nowrap">
+          <p>Open sourced on GitHub:</p>
+          <a href="https://github.com/Syazvinski/Altitude-Learning-Letter-Grade-Calculator-Mount-Vernon" target="_blank" rel="noopener noreferrer" className="underline text-blue-200">Extension Code</a>
+          <a href="https://github.com/Syazvinski/Altitude-Learning-Letter-Grade-Calculator-Website" target="_blank" rel="noopener noreferrer" className="underline text-blue-200">Website Code</a>
+        </div>
+      </div>
 
       {/* Footer */}
       <footer className="mt-auto py-4 whitespace-nowrap flex flex-col justify-center items-center">
         <p className="text-sm mb-2">made because you shouldn't need a chart to know your grade</p>
-        <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center space-x-4">
           <p className="mr-2">Made by former student, Stephan Yazvinski. &copy; 2024</p>
           <a href="https://www.linkedin.com/in/stephan-yazvinski-2086771b8/" target="_blank" rel="noopener noreferrer" className="underline text-blue-200">LinkedIn</a>
         </div>
